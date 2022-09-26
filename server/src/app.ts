5a224841167d935f8024fdeab8a6ea38a47dd17d
Jesper Grankvist <jesper.grankvist@hotmail.com>
2022-09-26 16:59:06 +0200

    Change structure of client dependencies

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,25 @@
 import express, { Application, json, Request, Response } from 'express'
 import cors from 'cors'
+import ChatMessage from "@my-chat-app/shared"
 
 const app: Application = express()
 app.use(cors())
 app.use(json())
 const port: number = parseInt(process.env.SERVER_PORT || "3001")
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('HomePage')
+app.get('/', (req: Request, res: Response<ChatMessage>) => {
+    console.log(" / route")
+    res.send({
+        id: "1",
+        author: "JG",
+        text: "Chat-msg",
+        timeStamp: new Date()
+    })
+})
+
+app.get('/test', (req: Request, res: Response) => {
+    console.log("test route")
+    res.send('test page')
 })
 
 app.listen(port, function () {
