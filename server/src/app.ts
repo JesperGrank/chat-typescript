28b3f2134de8dff2e3cd9c85902ff85892ea1ed6
Jesper Grankvist <jesper.grankvist@hotmail.com>
2022-09-29 22:46:12 +0200

    Connection between client/server - no database yet.

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,24 +2,27 @@ import express, { Application, json, Request, Response } from 'express'
 import cors from 'cors'
 import ChatMessage from "@my-chat-app/shared"
 
+import crypto from "crypto"
+
 const app: Application = express()
 app.use(cors())
 app.use(json())
 const port: number = parseInt(process.env.SERVER_PORT || "3001")
 
-app.get('/', (req: Request, res: Response<ChatMessage>) => {
-    console.log(" / route")
-    res.send({
-        id: "1",
-        author: "JG",
-        text: "Chat-msg",
-        timeStamp: new Date()
-    })
+const CHAT_MESSAGES: ChatMessage[] = [{id: "1", author: "Jeppe", text: "Första inlägget", timeStamp: new Date()}, {id: "2", author: "JG", text: "andra inlägg", timeStamp: new Date()}]
+
+app.get('/', (req: Request, res: Response<ChatMessage[]>) => {   
+    res.send(CHAT_MESSAGES)
 })
 
-app.get('/test', (req: Request, res: Response) => {
-    console.log("test route")
-    res.send('test page')
+app.post("/", (req: Request<ChatMessage>, res: Response<ChatMessage[]>) => {
+    const message = req.body
+    message.id = crypto.randomUUID()
+
+    console.log("Sent new message", message)
+    CHAT_MESSAGES.push(message)
+    res.send(CHAT_MESSAGES)
+
 })
 
 app.listen(port, function () {
