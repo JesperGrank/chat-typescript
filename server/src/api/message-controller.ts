262ae02828d99f3f4a5952237fa7d7f81b11772a
Jesper Grankvist <jesper.grankvist@hotmail.com>
2022-10-04 23:57:30 +0200

    Add file structure - controllers, repository + services

---
+++ server/src/api/message-controller.ts
@@ -0,0 +1,19 @@
+import ChatMessage from "@my-chat-app/shared"
+import express, {Request, Response } from "express"
+import { loadAllMessages } from "../models/messages-repository"
+import { saveMessageItem, loadAllMessagesItem } from "../services/messages-service"
+
+const messageController = express.Router()
+
+messageController.get("/", async (req: Request, res: Response<ChatMessage[]>) => {
+    // const messages = await loadAllMessages()
+    // console.log(messages)
+    res.send(await loadAllMessagesItem())
+})
+
+messageController.post("/", async (req: Request<ChatMessage>, res: Response<ChatMessage[]>) => {
+    res.send(await saveMessageItem(req.body))
+    
+})
+
+export default messageController
