1ab10754871f0c6bd28103bdff9120c90041bfda
Jesper Grankvist <jesper.grankvist@hotmail.com>
2022-10-12 23:18:11 +0200

    Bcrypt installed to salt + hash password

--- server/src/models/user-repository.ts
+++ server/src/models/user-repository.ts
@@ -1,5 +1,6 @@
 import {User} from "@my-chat-app/shared"
 import {model, Schema} from "mongoose"
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
     username: {type: String, required: true, unique: true},
@@ -10,6 +11,8 @@ const UserSchema = new Schema({
 const UserModel = model<User>("User", UserSchema)
 
 export const saveUser = async (User: User): Promise<void> => {
+    const salt = await bcrypt.genSalt(10)
+    User.password = await bcrypt.hash(User.password, salt)
     const newUser = new UserModel(User)
     newUser.save()
 }
