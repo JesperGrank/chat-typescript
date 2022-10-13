b563823ae15c96adc5300e2b32dfe1f33ee62686
Jesper Grankvist <jesper.grankvist@hotmail.com>
2022-10-13 23:01:29 +0200

    Add browserrouter to index + links in navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import {Link} from "react-router-dom"
 
 export default function Navbar() {
   return (
     <nav>
         <ul className="nav_links">
-        <li><a href='/register'>Register</a></li>
-        <li><a href='/login'>Login</a></li>
+        <li><Link to="/register">Register</Link></li>
+        <li><Link to="/login">Login</Link></li>
         </ul>
     </nav>
   )
