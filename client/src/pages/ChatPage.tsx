48e83321c43adf389aa3ed602ceee93292387ce8
Jesper Grankvist <jesper.grankvist@hotmail.com>
2022-10-28 15:30:29 +0200

    attempt

---
+++ client/src/pages/ChatPage.tsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from 'react'
+import { ChatMessage } from "@my-chat-app/shared"
+import axios from "axios"
+
+axios.defaults.baseURL = "http://localhost:3001"
+
+export default function HomePage() {
+  
+  const [chatMessage, setChatMessage] = useState<string>("")
+  // const [userName, setUserName] = useState<string>("")
+  const [messages, setMessages] = useState<ChatMessage[]>([])
+  const [error, setError] = useState<string | undefined>("")
+  
+  const fetchMessages = async (): Promise<ChatMessage[]> => {
+    const reponse = await axios.get<ChatMessage[]>("/")
+    return reponse.data
+  }
+  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetchMessages()
+      .then(setMessages)
+      .catch((error) => {
+        setMessages([])
+        setError("Something went wrong when fetching messages...")
+      })
+    }, 2500)
+  }, [])
+  
+  const createMessage = async (chatMessage: string): Promise<void> => {
+    const username = localStorage.getItem("ts-webchat")
+    const message: ChatMessage = {
+      text: chatMessage,
+      author: username,
+      timeStamp: new Date()
+    }
+    try {
+      const response = await axios.post<ChatMessage[]>("/", message)
+      setMessages(response.data)
+    } catch (error) {
+      setMessages([])
+      setError("Invalid message input")
+    }
+
+  }
+  const output = () => {
+    if (error) {
+      return (<div>{error}</div>)
+    } else if (messages) {
+      return (<div>
+        {messages && messages.map((singleMessage, index) => {
+          return (
+            <div key={index}>
+              {singleMessage.author}: {singleMessage.text} <br />
+              {singleMessage.timeStamp.toString().split('T')[0].substring(0, 10)} - {singleMessage.timeStamp.toString().split('T')[1].substring(0, 5)}
+            </div>
+          )
+        })}
+      </div>)
+    } else {
+      <div>'Something went wrong fetching messages...'</div>
+    }
+  }
+  return (
+
+    <div>
+      <h1> This is "ChatRoom"</h1>
+      {output()}
+      <section>
+        <input type="text" placeholder="Message" value={chatMessage} onChange={(e) => setChatMessage(e.target.value)} />
+        <button onClick={(e) => createMessage(chatMessage)}>Send message</button>
+      </section>
+    </div>
+  )
+}
