56be013e06ce6c451575f729347931a6df3397e6
Jesper Grankvist <jesper.grankvist@hotmail.com>
2022-10-28 12:31:00 +0200

    add error handling for messages + interval for fetching new messages

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -4,45 +4,77 @@ import axios from "axios"
 
 axios.defaults.baseURL = "http://localhost:3001"
 
-const fetchMessages = async (): Promise <ChatMessage[]> => {
-  const reponse = await axios.get<ChatMessage[]>("/")
-  return reponse.data
-}
-
 export default function HomePage() {
 
     const [chatMessage, setChatMessage] = useState<string>("")
     const [userName, setUserName] = useState<string>("")
     const [messages, setMessages] = useState<ChatMessage[]>([])
+    const [error, setError] = useState<string | undefined>("")
+
+    const fetchMessages = async (): Promise <ChatMessage[]> => {
+      const reponse = await axios.get<ChatMessage[]>("/")
+      return reponse.data
+    }
   
     useEffect(() => {
-      fetchMessages().then(setMessages)
-    },[])
+      const interval = setInterval(() => {
+        fetchMessages()
+          .then(setMessages)
+          .catch((error) => {
+            setMessages([])
+            setError("Something went wrong when fetching messages...")
+          })
+      }, 2500)
+    }, [])
   
-    const sendMessage = (chatMessage: string): void => {
+    const createMessage = async (chatMessage: string, userName: string): Promise<void> => {
       const message: ChatMessage = {
         text: chatMessage,
         author: userName,
         timeStamp: new Date()
       }
-      axios.post<ChatMessage[]>("/", message)
-      .then((response) => setMessages(response.data))
+      try{
+        const response = await axios.post<ChatMessage[]>("/", message)
+        setMessages(response.data)
+      } catch (error){
+        setMessages([])
+        setError("Invalid username and message input. Both needs to have a value.")
+      }
+
     }
-  return (
-    <div>
+    const output = () => {
+      if(error){
+          return (<div>{error}</div>)
+      } else if(messages) {
+        return (<div>
         {messages && messages.map((singleMessage, index) => {
         return (
           <div key={index}>
             {singleMessage.author}: {singleMessage.text} <br/>
             {singleMessage.timeStamp.toString().split('T')[0].substring(0,10)} - {singleMessage.timeStamp.toString().split('T')[1].substring(0, 5)}
           </div>
-        )
+        ) 
       })}
-
+      </div>)
+      } else{
+        <div>'Something went wrong fetching messages...'</div>
+      }
+    }
+  return (
+    <div>
+        {/* {messages && messages.map((singleMessage, index) => {
+        return (
+          <div key={index}>
+            {singleMessage.author}: {singleMessage.text} <br/>
+            {singleMessage.timeStamp.toString().split('T')[0].substring(0,10)} - {singleMessage.timeStamp.toString().split('T')[1].substring(0, 5)}
+          </div>
+        )
+      })} */}
+      {output()}
       <section>
         <input type="text" placeholder='Username' value={userName} onChange={(e) => setUserName(e.target.value)}/>
         <input type="text" placeholder="Message" value={chatMessage} onChange={(e) => setChatMessage(e.target.value)}/>
-        <button onClick={(e) => sendMessage(chatMessage)}>Send message</button>  
+        <button onClick={(e) => createMessage(chatMessage, userName)}>Send message</button>  
       </section>
     </div>
   )
